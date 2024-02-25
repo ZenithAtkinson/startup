8dcffba016e1351134a29371aff030eaa2b2128a
zenith <alexanderdavidatkinson@gmail.com>
2024-02-25 14:43:53 -0700

    finishing login

--- startup-code/login.js
+++ startup-code/login.js
@@ -4,4 +4,12 @@ function login() {
     localStorage.setItem("userName", nameEl.value);
     window.location.href = "index.html";
   }
-  
+  function updateWelcomeMessage() {
+    const userName = localStorage.getItem("userName");
+    if (userName) {
+      document.querySelector("#welcome-message").textContent = `Welcome, ${userName}`;
+    }
+  }
+  document.addEventListener('DOMContentLoaded', function() {
+    updateWelcomeMessage();
+  });

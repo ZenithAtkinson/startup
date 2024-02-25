4f134622184f5657a4c8e5e6ebc959e3e5d4ea48
zenith <alexanderdavidatkinson@gmail.com>
2024-02-25 14:53:13 -0700

    new user page + login changes

--- startup-code/login.js
+++ startup-code/login.js
@@ -3,13 +3,5 @@ function login() {
     const nameEl = document.querySelector("#name");
     localStorage.setItem("userName", nameEl.value);
     window.location.href = "index.html";
+    
   }
-  function updateWelcomeMessage() {
-    const userName = localStorage.getItem("userName");
-    if (userName) {
-      document.querySelector("#welcome-message").textContent = `Welcome, ${userName}`;
-    }
-  }
-  document.addEventListener('DOMContentLoaded', function() {
-    updateWelcomeMessage();
-  });

be19c878cedfe433fb2022045fc4206c0b49f6ce
zenith <alexanderdavidatkinson@gmail.com>
2024-03-28 11:42:01 -0600

    Websocket Scripts

---
+++ startup-code/peerProxy.js
@@ -0,0 +1,34 @@
+const { WebSocketServer } = require('ws');
+const uuid = require('uuid');
+
+function peerProxy(httpServer) {
+  // Create a websocket object
+  const wss = new WebSocketServer({ noServer: true });
+
+  // Handle the protocol upgrade from HTTP to WebSocket
+  httpServer.on('upgrade', (request, socket, head) => {
+    wss.handleUpgrade(request, socket, head, function done(ws) {
+      wss.emit('connection', ws, request);
+    });
+  });
+
+  // Keep track of all the connections so we can forward messages
+  let connections = [];
+
+  wss.on('connection', (ws) => {
+    const connection = { id: uuid.v4(), alive: true, ws: ws };
+    connections.push(connection);
+    //conections here:
+  });
+
+  ws.on('message', function message(data) {
+    connections.forEach((c) => {
+      if (c.id !== connection.id) {
+        c.ws.send(data); // This data should include the new event details
+      }
+    });
+  });
+
+}
+
+module.exports = {  };
